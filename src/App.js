e6bfffde63151468c4b7e8c49643f57f756f3d9e
pp <pyang33@illinois.edu>
2025-05-05 22:00:41 -0500

    Frontend only

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Event Board</h1>
+      <p>UIUC CS 498 Cloud Computing Applications</p>
+    </div>
+  );
+}
+
+export default App;
